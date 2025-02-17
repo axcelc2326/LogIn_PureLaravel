403d9e1f3de1fbdbdb29fd64d0aee19a7971034e
axcelc2326 <axcelc2326@gmail.com>
2025-02-17 15:11:02 +0800

    font black

--- resources/views/profile/index.blade.php
+++ resources/views/profile/index.blade.php
@@ -2,6 +2,6 @@
 
 @section('content')
     <div class="w-screen h-screen flex justify-center items-center">
-        <h1 class="text-9xl uppercase text-black font-medium">profile page</h1>
+        <h1 class="text-7xl uppercase text-black font-medium">profile page</h1>
     </div>
 @endsection
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,6 +2,6 @@
 
 @section('content')
     <div class="w-screen h-screen flex justify-center items-center">
-        <h1 class="text-9xl uppercase text-black font-medium">Landing Page</h1>
+        <h1 class="text-7xl uppercase text-black font-medium">Landing Page</h1>
     </div>
 @endsection
